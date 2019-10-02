23086118fcb310297d7c0676dac5b073eb77ccb6
Richard Zilahi <richard.zilahi@salesframe.com>
2019-10-02 23:49:48 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, HashRouter } from 'react-router-dom'
+
+import Home from './screens/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<HashRouter>
+			<Route path="/" exact component={Home} />
+		</HashRouter>
+	)
 }
 
-export default App;
+export default App
---
+++ src/screens/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './Home.module.scss'
+
+/**
+* @author zilahir
+* @function Homepage
+* */
+
+const Homepage = () => (
+	<div className={styles.homePageWrapper}>Homepage</div>
+)
+
+export default Homepage

29bcad9131120993a0ae315fb1b0d6bc8f0e5496
Richard Zilahi <richard.zilahi@salesframe.com>
2019-10-03 15:17:02 +0300

    implementing footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { footerMenu } from '../api/footer'
 import styles from './Footer.module.scss'
 
 /**
@@ -7,20 +8,34 @@ import styles from './Footer.module.scss'
 * @function Footer
 * */
 
-const Footer = () => (
-	<footer className={styles.footerWrapper}>
-		<div className={styles.socialContainer}>
-			<ul>
-				<li>
-					facebook
-				</li>
-				<li>
-					whatever
-				</li>
-			</ul>
-		</div>
-	</footer>
-)
+const Footer = () => {
+	const { legalText } = footerMenu.getFooterTexts()[0]
+	const { rights } = footerMenu.getFooterTexts()[1]
+	return (
+		<footer className={styles.footerWrapper}>
+			<div className={styles.socialContainer}>
+				<ul>
+					<li>
+						facebook
+					</li>
+					<li>
+						whatever
+					</li>
+				</ul>
+			</div>
+			<div className={styles.legalContainer}>
+				<p>
+					{ rights }
+				</p>
+				<small>
+					{
+						legalText
+					}
+				</small>
+			</div>
+		</footer>
+	)
+}
 
 
 export default Footer
--- src/components/api/footerMenu.js
+++
@@ -1,7 +0,0 @@
-export const footerMenu = {
-	menuItems: [
-		{ key: 1, label: 'Home', slug: 'home' },
-		{ key: 2, label: 'About us', slug: 'aboutus' },
-	],
-	getFooterMenu: () => footerMenu.menuItems,
-}

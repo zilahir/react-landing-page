b27402fd9884907a9817c8bdc166a4b9bdc1efe9
Richard Zilahi <richard.zilahi@salesframe.com>
2019-10-03 15:55:38 +0300

    headerH1 component introduced

---
+++ src/components/common/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/**
+* @author zilahir
+* @function Header
+* */
+
+const HeaderH1 = props => {
+	const { text, className } = props
+	return (
+		<h1 className={className}>
+			{text}
+		</h1>
+	)
+}
+
+HeaderH1.defaultProps = {
+	className: null,
+}
+
+HeaderH1.propTypes = {
+	className: PropTypes.string,
+	text: PropTypes.string.isRequired,
+}
+
+export default HeaderH1

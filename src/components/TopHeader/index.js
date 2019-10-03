aeca4ad4b2d05fa76a68f902b5d187972584f0aa
Richard Zilahi <richard.zilahi@salesframe.com>
2019-10-03 09:13:10 +0300

    renamed header > topheader

---
+++ src/components/TopHeader/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+/**
+* @author
+* @function TopHeader
+* */
+
+const TopHeader = () => (
+	<div>TopHeader</div>
+)
+
+export default TopHeader
---
+++ src/components/api/headerMenu.js
@@ -0,0 +1,3 @@
+export const headerMenu = [
+	{ label: 'Home', slug: 'home' },
+]

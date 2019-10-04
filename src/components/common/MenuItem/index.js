fcf5c5f4964e32aad3792305fc959394b1acea7b
Richard Zilahi <richard.zilahi@salesframe.com>
2019-10-04 08:51:03 +0300

    hader menuitems animation

--- src/components/common/MenuItem/index.js
+++ src/components/common/MenuItem/index.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './MenuItem.module.scss'
 
 /**
-* @author martincserep
+* @author martincserep, zilahir
 * @function MenuItem
 * */
 
 const MenuItem = props => {
 	const { label } = props
+	const [hovered, setHovered] = useState(false)
+	const toggleHover = () => setHovered(!hovered)
+
 	return (
-		<li className={styles.menuItem}>
+		<li
+			className={`${styles.menuItem} ${hovered ? styles.hoverOn : styles.hoverOff}`}
+			onMouseEnter={() => toggleHover()}
+			onMouseLeave={() => toggleHover()}
+		>
 			{label}
 		</li>
 	)
 }
-
 
 MenuItem.propTypes = {
 	label: PropTypes.string.isRequired,

c0c1156df2ea16ccf2bec3f09c0f3786271aa9f9
Richard Zilahi <richard.zilahi@salesframe.com>
2019-10-05 19:01:33 +0300

    implementing images

--- src/components/common/Image/index.js
+++ src/components/common/Image/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import styles from './Image.module.scss'
 
@@ -8,20 +9,26 @@ import styles from './Image.module.scss'
 * @function Image
 * */
 
+const Img = styled.img`
+	max-width: ${props => props.maxWidth}px;
+`
+
 const Image = props => {
-	const { src, className, alt } = props
+	const { src, className, alt, maxWidth } = props
 	return (
-		<img src={src} className={className} alt={alt} />
+		<Img maxWidth={maxWidth} src={src} className={className} alt={alt} />
 	)
 }
 
 Image.defaultProps = {
 	className: styles.imageStyle,
+	maxWidth: null,
 }
 
 Image.propTypes = {
 	alt: PropTypes.string.isRequired,
 	className: PropTypes.string,
+	maxWidth: PropTypes.number,
 	src: PropTypes.string.isRequired,
 }
 
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -7,7 +7,11 @@ import Footer from '../../components/Footer'
 import Section from '../../components/common/Section'
 import HeaderH1 from '../../components/common/HeaderH1'
 import Paragraph from '../../components/common/Paragraph'
+import Image from '../../components/common/Image'
 import styles from './Home.module.scss'
+import Image1 from '../../assets/img/girl_on_phone.jpg'
+// import Image2 from '../../assets/img/girl_reading_menu.jpg'
+// import Image3 from '../../assets/img/phone_and_food.jpg'
 
 /**
 * @author zilahir
@@ -27,6 +31,7 @@ const Homepage = () => {
 					<Section imagePos="left">
 						<HeaderH1 text="helloo" />
 						<Paragraph text="Exercitation pariatur fugiat incididunt esse consequat officia ut dolore. Voluptate ad nisi et fugiat reprehenderit cupidatat ipsum commodo proident veniam veniam ipsum magna et. Amet ipsum adipisicing adipisicing nostrud id culpa velit. Velit aliqua voluptate cillum ea nulla cillum eu exercitation laborum velit cupidatat mollit." />
+						<Image src={Image1} maxWidth={400} alt="phone" />
 					</Section>
 				</div>
 				<Footer />

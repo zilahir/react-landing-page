3efb4ebea94c555fd6d7b2823d5d2e922224a466
Martin Cserép <martincserep1@gmail.com>
2019-10-05 20:42:22 +0200

    Testimonal style mod

--- src/components/api/testimonials.js
+++ src/components/api/testimonials.js
@@ -2,6 +2,9 @@ export const testimonials = {
 	testimonialItems: [
 		{ key: 1, src: 'puisto.png', alt: 'ravintola puisto' },
 		{ key: 1, src: 'puisto.png', alt: 'ravintola puisto' },
+		{ key: 1, src: 'puisto.png', alt: 'ravintola puisto' },
+		{ key: 1, src: 'puisto.png', alt: 'ravintola puisto' },
+		{ key: 1, src: 'puisto.png', alt: 'ravintola puisto' },
 	],
 	getTestimonials: () => testimonials.testimonialItems,
 }

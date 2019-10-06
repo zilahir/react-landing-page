fa9172ea6afb61315e1717a25e221961c99b8691
Richard Zilahi <richard.zilahi@salesframe.com>
2019-10-06 16:37:12 +0300

    parallax

--- src/components/common/Image/index.js
+++ src/components/common/Image/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
+import { Parallax } from 'react-parallax'
 
 import styles from './Image.module.scss'
 
@@ -21,7 +22,23 @@ const Image = props => {
 			{
 				secondaryImage
 					? (
-						<img className={styles.sImage} alt={alt} src={secondaryImage} />
+						<Parallax
+							bgImage={
+								secondaryImage
+							}
+							strength={300}
+							style={{
+								height: '300px',
+								width: '300px',
+								position: 'absolute',
+								top: '-30px',
+							}}
+							bgImageStyle={{
+								position: 'absolute',
+								width: '400px',
+								height: '200px',
+							}}
+						/>
 					)
 					: null
 			}

4da72089b60df9f543042598e146470d6c222242
Richard Zilahi <richard.zilahi@salesframe.com>
2019-10-06 19:24:40 +0300

    fixes

--- src/screens/Contact/index.js
+++ src/screens/Contact/index.js
@@ -31,38 +31,32 @@ const Contactpage = () => {
 					<Paragraph text="Test" />
 				</Col>
 				<Col lg={6} xs={12}>
-					<div>
-						<Parallax
-							bgImage={
-								purpleDots
-							}
-							strength={300}
-							style={{
-								height: '300px',
-								width: '300px',
-								position: 'absolute',
-								top: '150px',
-								right: '100px',
-							}}
-							bgImageStyle={{
-								position: 'absolute',
-								width: '300px',
-								height: '300px',
-							}}
-						/>
-						<motion.div
-							initial={{ x: 0 }}
-							animate={{ x: 100 }}
-						/>
-						<div className={styles.contactForm}>
-							<Input type="text" placeholder="Your name" />
-							<div>
-								<Input onChange={e => setName(e.target.value)} type="text" placeholder="Your name" />
-								<Input type="email" placeholder="Your e-mail" />
-								<Input type="text" placeholder="Your message" />
-								<div className={styles.btnContainer}>
-									<Button text="Contact Us" />
-								</div>
+					<Parallax
+						bgImage={
+							purpleDots
+						}
+						strength={300}
+						style={{
+							height: '300px',
+							width: '300px',
+							position: 'absolute',
+							top: '150px',
+							right: '100px',
+						}}
+						bgImageStyle={{
+							position: 'absolute',
+							width: '300px',
+							height: '300px',
+						}}
+					/>
+					<div className={styles.contactForm}>
+						<Input type="text" placeholder="Your name" />
+						<div>
+							<Input onChange={e => setName(e.target.value)} type="text" placeholder="Your name" />
+							<Input type="email" placeholder="Your e-mail" />
+							<Input type="text" placeholder="Your message" />
+							<div className={styles.btnContainer}>
+								<Button text="Contact Us" />
 							</div>
 						</div>
 					</div>

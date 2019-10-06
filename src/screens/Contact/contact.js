d678fe1c3a43f4e87f246a72d0600246b3ed365d
Martin Cserép <martincserep1@gmail.com>
2019-10-06 16:27:54 +0200

    contact page is getting started

---
+++ src/screens/Contact/contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import HeaderH1 from '../../components/common/HeaderH1'
+
+/**
+* @author martincserep
+* @function Contactpage
+* */
+
+// eslint-disable-next-line arrow-body-style
+const Contactpage = () => {
+	return (
+		<HeaderH1 key="header" text="helloo" />
+	)
+}
+
+export default Contactpage

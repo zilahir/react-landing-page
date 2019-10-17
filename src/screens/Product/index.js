debea765d9e9d3ee73cb53f7d112f2f74866965c
Richard Zilahi <richard.zilahi@salesframe.com>
2019-10-17 19:01:51 +0300

    implementing product page

--- src/screens/Product/index.js
+++ src/screens/Product/index.js
@@ -48,6 +48,35 @@ const Product = () => (
 				</div>
 			</Col>
 		</Row>
+		<Row>
+			<div className={styles.functionContainer}>
+				<Col
+					lg={12}
+					xs={12}
+					lgOffset={1}
+				>
+					<Section imagePos="right">
+						<HeaderH2 className={styles.subHeader} key="header" text="introducing our app" />
+						<HeaderH1 key="header" text="What we offer" />
+						<Paragraph key="p" className={styles.paragraph} text="Veniam tempor exercitation consectetur deserunt esse excepteur elit adipisicing. Officia laboris id tempor aliquip ullamco ullamco amet ex occaecat est eiusmod. Magna nisi veniam eiusmod nostrud aliqua aliqua reprehenderit anim. Excepteur est cillum sunt deserunt nostrud laborum in dolor nostrud ut Lorem nisi. Ut deserunt officia laborum proident ad qui consectetur dolore incididunt. Cupidatat nulla ea id irure labore consequat laboris cupidatat reprehenderit qui pariatur laboris magna." />
+						<Phone
+							key="image"
+							className={styles.phoneAnimation}
+							animationEnd={styles.resetAnimation}
+						>
+							<div style={{
+								backgroundImage: `url(${Screen1})`,
+								width: '100%',
+								height: '100%',
+								backgroundSize: 'cover',
+								backgroundPosition: '50% 50%',
+							}}
+							/>
+						</Phone>
+					</Section>
+				</Col>
+			</div>
+		</Row>
 		<Footer />
 	</Grid>
 )

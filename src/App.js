036733f0dcf3c88666f3b07a39889fd11fad846d
Martin Cserép <martincserep1@gmail.com>
2019-10-27 22:51:46 +0100

    jobs screen created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './screens/Home'
 import Contact from './screens/Contact'
 import About from './screens/About'
 import Product from './screens/Product'
+import Jobs from './screens/Jobs'
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 			<Route path="/contact" exact component={Contact} />
 			<Route path="/about" exact component={About} />
 			<Route path="/product" exact component={Product} />
+			<Route path="/jobs" exact component={Jobs} />
 		</HashRouter>
 	)
 }
---
+++ src/screens/Jobs/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Grid, Row, Col } from 'react-flexbox-grid'
+
+import TopHeader from '../../components/TopHeader'
+import Footer from '../../components/Footer'
+import Section from '../../components/common/Section'
+import HeaderH1 from '../../components/common/HeaderH1'
+import HeaderH2 from '../../components/common/HeaderH2'
+import Paragraph from '../../components/common/Paragraph'
+import styles from './Jobs.module.scss'
+import Screen1 from '../../assets/img/phonescreens/screen_1.png'
+import GreyDots from '../../assets/img/misc/grey_dots.svg'
+
+/**
+* @author martincserep
+* @function Jobs
+* */
+
+const Jobs = () => (
+	<Grid
+		fluid
+		className={styles.fluid}
+	>
+		<TopHeader />
+
+		<Footer />
+	</Grid>
+)
+
+export default Jobs

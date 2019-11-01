51e8d1a60903a95226b095d2130ba2eafdc38937
Richard Zilahi <richard.zilahi@salesframe.com>
2019-11-01 23:42:50 +0200

    implementing burger menu

---
+++ src/components/BurgerMenu/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { slide as Menu } from 'react-burger-menu'
+import { menu as MenuIcon } from 'react-icons-kit/feather/menu'
+import Icon from 'react-icons-kit'
+
+import styles from './BurgerMenu.module.scss'
+
+/**
+* @author zilahir
+* @function BurgerMenu
+* */
+
+const BurgerMenu = () => {
+	const [isBurgerMenuOpen, setBurgerMenuOpen] = useState(false)
+	return (
+		<div>
+			<div
+				onClick={() => setBurgerMenuOpen(!isBurgerMenuOpen)}
+				onKeyPress={() => null}
+				role="button"
+				tabIndex={0}
+				className={styles.burgerButton}
+			>
+				<Icon icon={MenuIcon} />
+			</div>
+			<Menu
+				isOpen={isBurgerMenuOpen}
+				customBurgerIcon={false}
+				itemListClassName={styles.menu}
+			>
+				<Link to="about">
+					About
+				</Link>
+			</Menu>
+		</div>
+	)
+}
+
+export default BurgerMenu

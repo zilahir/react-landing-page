077544de10c374c8e25fbdd8bc8866cfa3f0f3eb
Richard Zilahi <richard.zilahi@salesframe.com>
2019-11-02 22:42:17 +0200

    gatsby-plugin-sass added to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,15 @@ module.exports = {
 	},
 	plugins: [
 		{
+			resolve: 'gatsby-plugin-sass',
+			options: {
+				// Override the file regex for SASS
+				sassRuleTest: /\.global\.s(a|c)ss$/,
+				// Override the file regex for CSS modules
+				sassRuleModulesTest: /\.mod\.s(a|c)ss$/,
+			},
+		},
+		{
 			resolve: 'gatsby-source-graphql',
 			options: {
 				// This type will contain remote schema Query type
